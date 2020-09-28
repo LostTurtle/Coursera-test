3100c60e6839d57675911fbd1cec8f6483e9fd52
LostTurtle <khaloodm25@gmail.com>
2020-09-28 14:46:20 +0200

    jquery

---
+++ Aboutus/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({interval: 1000})
+});
+$('#carousel-pause').click(function() {
+    $('#mycarousel').carousel('pause')
+});
+$('#carousel-play').click(function() {
+    $('#mycarousel').carousel('play')
+});
+$(document).ready(function (){
+    $('#mycarousel').carousel({
+        pause: "hover"
+    })
+});
